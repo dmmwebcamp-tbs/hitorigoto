7b7680fcf80d01028d6776b95e253e6b423db558
HirokiKawai <169style.hckq@gmail.com>
2019-03-18 03:59:16 +0000

    確認

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   registrations: 'users/registrations'
 }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'credit' => 'payments#credit'
+
 end
